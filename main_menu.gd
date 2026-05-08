0d512245f53a8c9d9e1684598c3913f8538ad1af
Yiping <yiping940116@gmail.com>
2026-05-08 11:54:28 +0800

    遊戲開始介面上下選單

--- main_menu.gd
+++ main_menu.gd
@@ -10,6 +10,10 @@ extends Control
 @onready var volume_slider = $SettingsPanel/HSlider
 @onready var back_button = $SettingsPanel/Button
 
+var menu_buttons: Array[Button] = []
+var selected_menu_index := 0
+
+
 func _ready():
 	_play_title_music()
 
@@ -26,6 +30,8 @@ func _ready():
 
 	settings_panel.visible = false
 	continue_button.disabled = not GameState.has_continue_scene()
+	menu_buttons = [start_button, continue_button, quit_button]
+	_setup_menu_keyboard()
 
 func _on_start_pressed():
 	if FileAccess.file_exists("user://save_game.json"):
@@ -52,6 +58,64 @@ func _on_back_pressed():
 
 func _on_volume_changed(value):
 	AudioServer.set_bus_volume_db(0, linear_to_db(value))
+
+
+func _input(event: InputEvent) -> void:
+	if settings_panel.visible:
+		return
+
+	if event.is_action_pressed("ui_up"):
+		_move_menu_selection(-1)
+		_mark_input_handled()
+	elif event.is_action_pressed("ui_down"):
+		_move_menu_selection(1)
+		_mark_input_handled()
+	elif event.is_action_pressed("ui_accept"):
+		_mark_input_handled()
+		var selected_button := menu_buttons[selected_menu_index]
+		if not selected_button.disabled:
+			selected_button.emit_signal("pressed")
+
+
+func _setup_menu_keyboard() -> void:
+	for i in range(menu_buttons.size()):
+		var button := menu_buttons[i]
+		button.focus_mode = Control.FOCUS_ALL
+		button.focus_entered.connect(_on_menu_button_focus_entered.bind(i))
+
+	_select_first_enabled_menu_button()
+
+
+func _select_first_enabled_menu_button() -> void:
+	for i in range(menu_buttons.size()):
+		if not menu_buttons[i].disabled:
+			selected_menu_index = i
+			menu_buttons[i].grab_focus()
+			return
+
+
+func _move_menu_selection(direction: int) -> void:
+	if menu_buttons.is_empty():
+		return
+
+	var next_index := selected_menu_index
+	for i in range(menu_buttons.size()):
+		next_index = wrapi(next_index + direction, 0, menu_buttons.size())
+		if not menu_buttons[next_index].disabled:
+			selected_menu_index = next_index
+			menu_buttons[selected_menu_index].grab_focus()
+			return
+
+
+func _on_menu_button_focus_entered(index: int) -> void:
+	selected_menu_index = index
+
+
+func _mark_input_handled() -> void:
+	var viewport := get_viewport()
+	if viewport != null:
+		viewport.set_input_as_handled()
+
 
 func _on_background_video_finished():
 	background_video.stream_position = 0.0
